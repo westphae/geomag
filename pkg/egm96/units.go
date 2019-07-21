73fbea791ff57a8133db37561452a5ed7a7e05c8
Eric Westphal <BonanzaEric@gmail.com>
2019-07-21 11:59:28 -0400

    Add some unit conversions for EGM96 interpolation

---
+++ pkg/egm96/units.go
@@ -0,0 +1,40 @@
+package egm96
+
+type Meters float64
+
+type Degrees float64
+
+type Location struct {
+	Latitude Degrees
+	Longitude Degrees
+	Height Meters
+}
+
+func MetersFromFeet(ft float64) (m Meters) {
+	return Meters(0.3048*ft)
+}
+
+func (m Meters) ToFeet() (ft float64) {
+	return float64(m)/0.3048
+}
+
+func DegreesFromDMS(d, m, s float64) (dd Degrees) {
+	var sgn float64 = 1
+	if d<0 {
+		sgn = -1
+	}
+	return Degrees(d+sgn*(m+s/60)/60)
+}
+
+func (dd Degrees) ToDMS() (d, m, s float64) {
+	var sgn float64 = 1
+	if dd<0 {
+		sgn = -1
+	}
+	z := float64(dd)
+	d = float64(int(z))
+	z = (z-d)*60
+	m = float64(int(z))
+	s = (z-m)*60
+	return d, sgn*m, sgn*s
+}
---
+++ test/egm96_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/westphae/geomag/pkg/egm96"
+)
+
+func TestMeters(t *testing.T) {
+	ms := []egm96.Meters{1, 1.5, -0.01}
+	fts := []float64{3.280840, 4.921260, -0.03280840}
+
+	for i, m := range ms {
+		ft := m.ToFeet()
+		if ft - fts[i] < -EPS || ft - fts[i] > EPS {
+			t.Errorf("Failure converting %6.1f m to ft, expected %6.1f, got %6.1f",
+				m, fts[i], ft)
+		}
+		mm := egm96.MetersFromFeet(ft)
+		if mm - m < -EPS || mm - m > EPS {
+			t.Errorf("Failure converting %6.1f m to ft and back, got %6.1f",
+				m, mm)
+		}
+	}
+
+	for i, ft := range fts {
+		m := egm96.MetersFromFeet(ft)
+		if m - ms[i] < -EPS || m - ms[i] > EPS {
+			t.Errorf("Failure converting %6.1f ft to m, expected %6.1f, got %6.1f",
+				ft, ms[i], m)
+		}
+		fft := m.ToFeet()
+		if fft - ft < -EPS || fft - ft > EPS {
+			t.Errorf("Failure converting %6.1f ft to m and back, got %6.1f",
+				ft, fft)
+		}
+	}
+}
+
+func TestDegrees(t *testing.T) {
+	ds := []float64{59, 30, 20, -12, -89}
+	ms := []float64{59, 12, 18, 45, 59}
+	ss := []float64{59.999, 46, 31, 12, 1.25}
+	dds := []egm96.Degrees{59.999999722, 30.212777777, 20.308611111, -12.753333333, -89.983680555}
+
+	for i, dd := range dds {
+		d, m, s := dd.ToDMS()
+		if d - ds[i] < -EPS || d - ds[i] > EPS ||
+			m - ms[i] < -EPS*60 || m - ms[i] > EPS*60 ||
+			s - ss[i] < -EPS*3600 || s - ss[i] > EPS*3600 {
+			t.Errorf("Failure converting %6.1f degrees to dms, expected %3.1f %2.1f %2.6f, got %3.1f %2.1f %2.6f",
+				dd, ds[i], ms[i], ss[i], d, m, s)
+		}
+		ddd := egm96.DegreesFromDMS(d, m, s)
+		if ddd - dd < -EPS || ddd - dd > EPS {
+			t.Errorf("Failure converting %6.1f degrees to dms and back, got %6.1f",
+				dd, ddd)
+		}
+	}
+
+}
