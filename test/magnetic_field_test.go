50956ca2920acea435da4af63a61d428a8ba70c4
Eric Westphal <BonanzaEric@gmail.com>
2019-07-26 22:29:54 -0400

    Test for full set of test data from paper, passes

--- test/magnetic_field_test.go
+++ test/magnetic_field_test.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"github.com/westphae/geomag/pkg/units"
 	"github.com/westphae/geomag/pkg/wmm"
+	"io/ioutil"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -77,4 +82,119 @@ func TestMagneticFieldFromPaperDetail(t *testing.T) {
 	testDiff("DH", wmm.MagneticField(mag).DH(), 16.5720479716, 1e-10, t)
 	testDiff("DD", wmm.MagneticField(mag).DD(), -0.0015009297/wmm.Deg, 1e-10, t)
 	testDiff("DI", wmm.MagneticField(mag).DI(), 0.0007945653/wmm.Deg, 1e-10, t)
+}
+
+func TestAllTestValuesFromPaper(t *testing.T) {
+	var (
+		date wmm.DecimalYear
+		height units.Meters
+		lat, lon units.Degrees
+		x, y, z float64
+		h, f, i, d float64
+		//gv float64
+		xdot, ydot, zdot float64
+		hdot, fdot, idot, ddot float64
+		data []byte
+		dat []string
+		err  error
+	)
+
+	data, err = ioutil.ReadFile("data/WMM2015v2TestValues.txt")
+	scanner := bufio.NewScanner(bytes.NewReader(data))
+	// Read and parse header
+	if !scanner.Scan() {
+		panic(err)
+	}
+	_ = strings.Fields(scanner.Text()) // Not using the header
+	for scanner.Scan() {
+		dat = strings.Fields(scanner.Text())
+		dd, err := strconv.ParseFloat(dat[0], 64)
+		if err != nil {
+			panic(err)
+		}
+
+		date = wmm.DecimalYear(dd)
+		if dd, err = strconv.ParseFloat(dat[1], 64); err != nil {
+			panic(err)
+		}
+		height = units.Meters(dd*1000)
+		if dd, err = strconv.ParseFloat(dat[2], 64); err != nil {
+			panic(err)
+		}
+		lat = units.Degrees(dd)
+		if dd, err = strconv.ParseFloat(dat[3], 64); err != nil {
+			panic(err)
+		}
+		lon = units.Degrees(dd)
+		loc := wmm.Geodetic{Latitude: lat, Longitude: lon, Height: height}
+
+		magS := wmm.CalculateWMMMagneticField(loc.ToSpherical(), date.ToTime())
+		mag := magS.ToEllipsoidal(loc)
+
+		if x, err = strconv.ParseFloat(dat[4], 64); err != nil {
+			panic(err)
+		}
+		testDiff("X", wmm.MagneticField(mag).X, x, 0.1, t)
+		if y, err = strconv.ParseFloat(dat[5], 64); err != nil {
+			panic(err)
+		}
+		testDiff("Y", wmm.MagneticField(mag).Y, y, 0.1, t)
+		if z, err = strconv.ParseFloat(dat[6], 64); err != nil {
+			panic(err)
+		}
+		testDiff("Z", wmm.MagneticField(mag).Z, z, 0.1, t)
+		if h, err = strconv.ParseFloat(dat[7], 64); err != nil {
+			panic(err)
+		}
+		testDiff("H", wmm.MagneticField(mag).H(), h, 0.1, t)
+		if f, err = strconv.ParseFloat(dat[8], 64); err != nil {
+			panic(err)
+		}
+		testDiff("F", wmm.MagneticField(mag).F(), f, 0.1, t)
+		if i, err = strconv.ParseFloat(dat[9], 64); err != nil {
+			panic(err)
+		}
+		testDiff("I", wmm.MagneticField(mag).I(), i, 0.01, t)
+		if d, err = strconv.ParseFloat(dat[10], 64); err != nil {
+			panic(err)
+		}
+		testDiff("D", wmm.MagneticField(mag).D(), d, 0.01, t)
+		//if gv, err = strconv.ParseFloat(dat[11], 64); err != nil {
+		//	panic(err)
+		//}
+		//testDiff("GV", wmm.MagneticField(mag).GV(), gv, 0.01, t)
+		if xdot, err = strconv.ParseFloat(dat[12], 64); err != nil {
+			panic(err)
+		}
+		testDiff("Xdot", wmm.MagneticField(mag).DX, xdot, 0.1, t)
+		if ydot, err = strconv.ParseFloat(dat[13], 64); err != nil {
+			panic(err)
+		}
+		testDiff("Ydot", wmm.MagneticField(mag).DY, ydot, 0.1, t)
+		if zdot, err = strconv.ParseFloat(dat[14], 64); err != nil {
+			panic(err)
+		}
+		testDiff("Zdot", wmm.MagneticField(mag).DZ, zdot, 0.1, t)
+		if hdot, err = strconv.ParseFloat(dat[15], 64); err != nil {
+			panic(err)
+		}
+		testDiff("Hdot", wmm.MagneticField(mag).DH(), hdot, 0.1, t)
+		if fdot, err = strconv.ParseFloat(dat[16], 64); err != nil {
+			panic(err)
+		}
+		testDiff("Fdot", wmm.MagneticField(mag).DF(), fdot, 0.1, t)
+		if idot, err = strconv.ParseFloat(dat[17], 64); err != nil {
+			panic(err)
+		}
+		testDiff("Idot", wmm.MagneticField(mag).DI(), idot, 0.01, t)
+		if ddot, err = strconv.ParseFloat(dat[18], 64); err != nil {
+			panic(err)
+		}
+		testDiff("Ddot", wmm.MagneticField(mag).DD(), ddot, 0.01, t)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
 }
