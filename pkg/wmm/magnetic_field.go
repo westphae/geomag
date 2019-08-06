b9902a2c02d77db8dd3a55907e9b4708ba4ce3e0
Eric Westphal <BonanzaEric@gmail.com>
2019-08-05 23:13:36 -0400

    Refactor in preparation for handling iterations, fix A->AGeo, most tests passing

--- pkg/wmm/magnetic_field.go
+++ pkg/wmm/magnetic_field.go
@@ -1,11 +1,15 @@
 package wmm
 
 import (
+	"log"
 	"math"
 	"time"
 
 	"github.com/westphae/geomag/pkg/polynomial"
+	"github.com/westphae/geomag/pkg/units"
 )
+
+const AGeo units.Meters = 6371200 // Geomagnetic Reference Radius
 
 type MagneticField struct {
 	X, Y, Z    float64
@@ -15,44 +19,6 @@ type MagneticField struct {
 type GeocentricMagneticField MagneticField
 
 type EllipsoidalMagneticField MagneticField
-
-func CalculateWMMMagneticField(loc Spherical, t time.Time) (field GeocentricMagneticField) {
-	phi := float64(loc.Latitude)*Deg
-	lambda := float64(loc.Longitude)*Deg
-	sinPhi := math.Sin(phi)
-	cosPhi := math.Cos(phi)
-	for n:=1; n<=MaxLegendreOrder; n++ {
-		nn := float64(n+1)
-		f := polynomial.Pow(float64(A/loc.Height), n+2)
-		for m:=0; m<=n; m++ {
-			mf := float64(m)
-			p := polynomial.LegendreFunction(n, m, sinPhi)
-			q := polynomial.LegendreFunction(n+1, m, sinPhi)
-			if m>0 {
-				p *= math.Sqrt(2*float64(polynomial.Factorial(n-m))/float64(polynomial.Factorial(n+m)))
-				q *= math.Sqrt(2*float64(polynomial.Factorial(n-m))/float64(polynomial.Factorial(n+m)))
-			}
-			dp := nn*math.Tan(phi)*p - math.Sqrt(nn*nn-mf*mf)/cosPhi*q
-			g, h, dg, dh, err := GetWMMCoefficients(n, m, Epoch.ToTime())
-			if err != nil {
-				panic(err)
-			}
-			sinMLambda := math.Sin(mf*lambda)
-			cosMLambda := math.Cos(mf*lambda)
-			field.X += -f*(g*cosMLambda+h*sinMLambda)*dp
-			field.Y += f/cosPhi*mf*(g*sinMLambda-h*cosMLambda)*p
-			field.Z += -nn*f*(g*cosMLambda+h*sinMLambda)*p
-			field.DX += -f*(dg*cosMLambda+dh*sinMLambda)*dp
-			field.DY += f/cosPhi*mf*(dg*sinMLambda-dh*cosMLambda)*p
-			field.DZ += -nn*f*(dg*cosMLambda+dh*sinMLambda)*p
-		}
-	}
-	dt := float64(TimeToDecimalYears(t)- Epoch)
-	field.X += dt*field.DX
-	field.Y += dt*field.DY
-	field.Z += dt*field.DZ
-	return field
-}
 
 func (m MagneticField) H() (h float64) {
 	return math.Sqrt(m.X*m.X + m.Y*m.Y)
@@ -102,4 +68,58 @@ func (m MagneticField) DD() (f float64) {
 
 func (m MagneticField) DGV() (f float64) {
 	return m.DD()
+}
+
+var (
+	curLoc   Spherical
+	curField GeocentricMagneticField
+)
+
+func CalculateWMMMagneticField(loc Spherical, t time.Time) (field GeocentricMagneticField) {
+	if loc.Longitude!=curLoc.Longitude || loc.Latitude!=curLoc.Latitude || loc.Height!=curLoc.Height {
+		curLoc = loc
+		curField = *new(GeocentricMagneticField)
+		phi := float64(loc.Latitude)*Deg
+		lambda := float64(loc.Longitude)*Deg
+		sinPhi := math.Sin(phi)
+		cosPhi := math.Cos(phi)
+		for n:=1; n<=MaxLegendreOrder; n++ {
+			nn := float64(n+1)
+			// if height varies, recalculate from here
+			f := polynomial.Pow(float64(AGeo/loc.Height), n+2)
+			for m:=0; m<=n; m++ {
+				mf := float64(m)
+				// if latitude varies, recalculate from here
+				p := polynomial.LegendreFunction(n, m, sinPhi)
+				q := polynomial.LegendreFunction(n+1, m, sinPhi)
+				if m>0 {
+					p *= math.Sqrt(2/polynomial.FactorialRatioFloat(n+m, n-m))
+					q *= math.Sqrt(2/polynomial.FactorialRatioFloat(n+m, n-m))
+				}
+				dp := nn*math.Tan(phi)*p - (nn-mf)/cosPhi*q
+				g, h, dg, dh, err := GetWMMCoefficients(n, m, Epoch.ToTime())
+				if err != nil {
+					panic(err)
+				}
+				// if longitude varies, recalculate from here
+				sinMLambda := math.Sin(mf*lambda)
+				cosMLambda := math.Cos(mf*lambda)
+				curField.X += -f*(g*cosMLambda+h*sinMLambda)*dp
+				curField.Y += f/cosPhi*mf*(g*sinMLambda-h*cosMLambda)*p
+				curField.Z += -nn*f*(g*cosMLambda+h*sinMLambda)*p
+				curField.DX += -f*(dg*cosMLambda+dh*sinMLambda)*dp
+				curField.DY += f/cosPhi*mf*(dg*sinMLambda-dh*cosMLambda)*p
+				curField.DZ += -nn*f*(dg*cosMLambda+dh*sinMLambda)*p
+				log.Printf("%d,%d: %f sum %f", n, m, f/cosPhi*mf*(g*sinMLambda-h*cosMLambda)*p, field.Y)
+			}
+		}
+	}
+	dt := float64(TimeToDecimalYears(t)- Epoch)
+	field.X = curField.X + dt*curField.DX
+	field.Y = curField.Y + dt*curField.DY
+	field.Z = curField.Z + dt*curField.DZ
+	field.DX = curField.DX
+	field.DY = curField.DY
+	field.DZ = curField.DZ
+	return field
 }
--- pkg/wmm/magnetic_field_test.go
+++ pkg/wmm/magnetic_field_test.go
@@ -11,17 +11,26 @@ import (
 	"github.com/westphae/geomag/pkg/units"
 )
 
+const (
+	epsM = 5e-5
+	red = "\u001b[31m"
+	green = "\u001b[32m"
+	reset = "\u001b[0m"
+)
+
 func testDiff(name string, actual, expected float64, eps float64, t *testing.T) {
-	if actual - expected > -eps && actual - expected < eps {
-		t.Logf("%s correct: expected %8.4f, got %8.4f", name, expected, actual)
-		return
-	}
-	t.Errorf("%s incorrect: expected %8.4f, got %8.4f", name, expected, actual)
+	//if !t.Failed() {
+		if actual-expected >= -eps && actual-expected <= eps {
+			t.Logf("%s%s correct: expected %8.4f, got %8.4f%s", green, name, expected, actual, reset)
+			return
+		}
+		t.Errorf("%s%s incorrect: expected %8.4f, got %8.4f%s", red, name, expected, actual, reset)
+	//}
 }
 
 func TestMagneticFieldFromPaperDetail(t *testing.T) {
 	// Test values in paper are only for original version of WMM-2015
-	LoadWMMCOF("test_data/WMM2015v1.COF")
+	LoadWMMCOF("testdata/WMM2015v1.COF")
 	tt := DecimalYear(2017.5)
 	loc := Geodetic{
 		Latitude: units.Degrees(-80),
@@ -29,61 +38,60 @@ func TestMagneticFieldFromPaperDetail(t *testing.T) {
 		Height: units.Meters(100e3),
 	}
 
-	testDiff("lambda", float64(loc.Longitude)*Deg, 4.1887902048, 1e-10, t)
-	testDiff("phi", float64(loc.Latitude)*Deg, -1.3962634016, 1e-10, t)
-	testDiff("h", float64(loc.Height), 100000.0000000000, 1e-10, t)
-	testDiff("t", float64(tt), 2017.5000000000, 1e-10, t)
+	testDiff("lambda", float64(loc.Longitude)*Deg, 4.1887902048, epsM, t)
+	testDiff("phi", float64(loc.Latitude)*Deg, -1.3962634016, epsM, t)
+	testDiff("h", float64(loc.Height), 100000.0000000000, epsM, t)
+	testDiff("t", float64(tt), 2017.5000000000, epsM, t)
 
 	locS := loc.ToSpherical()
-	testDiff("phi-prime", float64(locS.Latitude)*Deg, -1.3951289589, 1e-10, t)
-	testDiff("r", float64(locS.Height), 6457402.3484473705, 1e-10, t)
+	testDiff("phi-prime", float64(locS.Latitude)*Deg, -1.3951289589, epsM, t)
+	testDiff("r", float64(locS.Height), 6457402.3484473705, epsM, t)
 
 	var g, h float64
-	ghEps := 0.05
 	g, h, _, _, _ = GetWMMCoefficients(1, 0, tt.ToTime())
-	testDiff("g(1,0,t)", g, -29411.7500000000, ghEps, t)
-	testDiff("h(1,0,t)", h, 0.0000000000, ghEps, t)
+	testDiff("g(1,0,t)", g, -29411.7500000000, epsM, t)
+	testDiff("h(1,0,t)", h, 0.0000000000, epsM, t)
 
 	g, h, _, _, _ = GetWMMCoefficients(1, 1, tt.ToTime())
-	testDiff("g(1,1,t)", g, -1456.3500000000, ghEps, t)
-	testDiff("h(1,1,t)", h, 4729.2000000000, ghEps, t)
+	testDiff("g(1,1,t)", g, -1456.3500000000, epsM, t)
+	testDiff("h(1,1,t)", h, 4729.2000000000, epsM, t)
 
 	g, h, _, _, _ = GetWMMCoefficients(2, 0, tt.ToTime())
-	testDiff("g(2,0,t)", g, -2466.8000000000, ghEps, t)
-	testDiff("h(2,0,t)", h, 0.0000000000, ghEps, t)
+	testDiff("g(2,0,t)", g, -2466.8000000000, epsM, t)
+	testDiff("h(2,0,t)", h, 0.0000000000, epsM, t)
 
 	g, h, _, _, _ = GetWMMCoefficients(2, 1, tt.ToTime())
-	testDiff("g(2,1,t)", g, 3004.2500000000, ghEps, t)
-	testDiff("h(2,1,t)", h, -2913.3500000000, ghEps, t)
+	testDiff("g(2,1,t)", g, 3004.2500000000, epsM, t)
+	testDiff("h(2,1,t)", h, -2913.3500000000, epsM, t)
 
 	g, h, _, _, _ = GetWMMCoefficients(2, 2, tt.ToTime())
-	testDiff("g(2,2,t)", g, 1682.6000000000, ghEps, t)
-	testDiff("h(2,2,t)", h, -675.2500000000, ghEps, t)
+	testDiff("g(2,2,t)", g, 1682.6000000000, epsM, t)
+	testDiff("h(2,2,t)", h, -675.2500000000, epsM, t)
 
 	magS := CalculateWMMMagneticField(locS, tt.ToTime())
-	testDiff("X-prime", magS.X, 5626.6068398092, 1e-10, t)
-	testDiff("Y-prime", magS.Y, 14808.8492023104, 1e-10, t)
-	testDiff("Z-prime", magS.Z, -50169.4287102381, 1e-10, t)
-	testDiff("Xprime-dot", magS.DX, 28.2627812813, 1e-10, t)
-	testDiff("Yprime-dot", magS.DY, 6.9411521726, 1e-10, t)
-	testDiff("Zprime-dot", magS.DZ, 86.2115570931, 1e-10, t)
+	testDiff("X-prime", magS.X, 5626.6068398092, epsM, t)
+	testDiff("Y-prime", magS.Y, 14808.8492023104, epsM, t)
+	testDiff("Z-prime", magS.Z, -50169.4287102381, epsM, t)
+	testDiff("Xprime-dot", magS.DX, 28.2627812813, epsM, t)
+	testDiff("Yprime-dot", magS.DY, 6.9411521726, epsM, t)
+	testDiff("Zprime-dot", magS.DZ, 86.2115570931, epsM, t)
 
 	mag := magS.ToEllipsoidal(loc)
-	testDiff("X", mag.X, 5683.5175495763, 1e-10, t)
-	testDiff("Y", mag.Y, 14808.8492023104, 1e-10, t)
-	testDiff("Z", mag.Z, -50163.0133654779, 1e-10, t)
-	testDiff("Xdot", mag.DX, 28.1649610434, 1e-10, t)
-	testDiff("Ydot", mag.DY, 6.9411521726, 1e-10, t)
-	testDiff("Zdot", mag.DZ, 86.2435641169, 1e-10, t)
+	testDiff("X", mag.X, 5683.5175495763, epsM, t)
+	testDiff("Y", mag.Y, 14808.8492023104, epsM, t)
+	testDiff("Z", mag.Z, -50163.0133654779, epsM, t)
+	testDiff("Xdot", mag.DX, 28.1649610434, epsM, t)
+	testDiff("Ydot", mag.DY, 6.9411521726, epsM, t)
+	testDiff("Zdot", mag.DZ, 86.2435641169, epsM, t)
 
-	testDiff("F", MagneticField(mag).F(), 52611.1423211683, 1e-10, t)
-	testDiff("H", MagneticField(mag).H(), 15862.0423159539, 1e-10, t)
-	testDiff("D", MagneticField(mag).D(), 1.2043399870/Deg, 1e-10, t)
-	testDiff("I", MagneticField(mag).I(), -1.2645351837/Deg, 1e-10, t)
-	testDiff("DF", MagneticField(mag).DF(), -77.2340297896, 1e-10, t)
-	testDiff("DH", MagneticField(mag).DH(), 16.5720479716, 1e-10, t)
-	testDiff("DD", MagneticField(mag).DD(), -0.0015009297/Deg, 1e-10, t)
-	testDiff("DI", MagneticField(mag).DI(), 0.0007945653/Deg, 1e-10, t)
+	testDiff("F", MagneticField(mag).F(), 52611.1423211683, epsM, t)
+	testDiff("H", MagneticField(mag).H(), 15862.0423159539, epsM, t)
+	testDiff("D", MagneticField(mag).D(), 1.2043399870/Deg, epsM, t)
+	testDiff("I", MagneticField(mag).I(), -1.2645351837/Deg, epsM, t)
+	testDiff("DF", MagneticField(mag).DF(), -77.2340297896, epsM, t)
+	testDiff("DH", MagneticField(mag).DH(), 16.5720479716, epsM, t)
+	testDiff("DD", MagneticField(mag).DD(), -0.0015009297/Deg, epsM, t)
+	testDiff("DI", MagneticField(mag).DI(), 0.0007945653/Deg, epsM, t)
 }
 
 func TestAllTestValuesFromPaper(t *testing.T) {
@@ -101,7 +109,7 @@ func TestAllTestValuesFromPaper(t *testing.T) {
 		err                    error
 	)
 
-	data, err = ioutil.ReadFile("test_data/WMM2015v2TestValues.txt")
+	data, err = ioutil.ReadFile("testdata/WMM2015v2TestValues.txt")
 	scanner := bufio.NewScanner(bytes.NewReader(data))
 	// Read and parse header
 	if !scanner.Scan() {
